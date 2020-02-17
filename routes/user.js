6570cebbdc79cd436f29d2aea16dc8812fdc640c
Brice LESNE <brice@mbp-de-brice.lan>
2020-02-17 15:33:44 +0100

    update(user.js): add express-formidable import

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const formidableMiddleware = require("express-formidable");
+router.use(formidableMiddleware());
 const uid2 = require("uid2");
 const SHA256 = require("crypto-js/sha256");
 const encBase64 = require("crypto-js/enc-base64");
